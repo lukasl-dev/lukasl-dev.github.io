8b567fb716f8027e68af5d87a4a3f97b95fb7b5b
lukasl-dev <git@lukasl.dev>
2020-11-23 13:20:10 +0100

    Add docs/

---
+++ docs/precache-manifest.1a40d01ae69c802a4c666334ba7efc0f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8373616fb1e853eed7c9",
+    "url": "/css/app.80eaf688.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0bfd11833607f4b08e5573d9afb12edb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5ea3d769e6cacab730",
+    "url": "/js/about.fe960957.js"
+  },
+  {
+    "revision": "8373616fb1e853eed7c9",
+    "url": "/js/app.9d44881c.js"
+  },
+  {
+    "revision": "bc94a742a3a2b3a6b696",
+    "url": "/js/chunk-vendors.0447e1da.js"
+  },
+  {
+    "revision": "460df3be374a47dbe9507e9d42661524",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
